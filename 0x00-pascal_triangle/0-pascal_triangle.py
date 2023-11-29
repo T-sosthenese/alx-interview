bf033300c2c3034ed21b3793bab136417053bfc1
sosthene <sosthenetimi94@gmail.com>
2023-11-30 01:01:12 +0300

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+from math import factorial
+
+
+def pascal_triangle(n):
+    """A function that computes Pascal's triangle coefficients."""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [factorial(i)//(factorial(j)*factorial(i-j)) for j in range(i+1)]
+        triangle.append(row)
+
+    return triangle
