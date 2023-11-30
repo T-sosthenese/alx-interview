a4862157901ef1ebaa37383cedf92ca383e472ec
sosthene <sosthenetimi94@gmail.com>
2023-12-01 00:48:34 +0300

    Pascal's triangle implementation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,10 +3,10 @@ from math import factorial
 
 def pascal_triangle(n):
     """A function that computes Pascal's triangle coefficients."""
-    if n <= 0:
-        return []
-
     triangle = []
+    if n <= 0:
+        return triangle
+
     for i in range(n):
         row = [factorial(i)//(factorial(j)*factorial(i-j)) for j in range(i+1)]
         triangle.append(row)
